c592d3a3b0f7d69fb53a73bdaa1b813c0330a95c
Nazarii Shvets <74127809+NazariiShvets@users.noreply.github.com>
2022-08-07 15:58:24 +0300

    chore: remove unused

--- src/lib/typescript.ts
+++ src/lib/typescript.ts
@@ -1,15 +1,5 @@
-type DeepPartial<T, Check = object> = {
-  [Key in keyof T]?: T[Key] extends Check ? DeepPartial<T[Key], Check> : T[Key];
-};
-
 type ShapeConfig<Shape, T> = {
   [Key in keyof Shape]: Shape[Key] extends T ? T : ShapeConfig<Shape[Key], T>;
-};
-
-type GetShape<TrimType, ReplaceType, Shape> = {
-  [Key in keyof Shape]: Shape[Key] extends TrimType
-    ? ReplaceType
-    : GetShape<TrimType, ReplaceType, Shape[Key]>;
 };
 
 type RequireSingleField<Shape, Key extends keyof Shape> = Required<
@@ -17,4 +7,4 @@ type RequireSingleField<Shape, Key extends keyof Shape> = Required<
 > &
   Omit<Shape, Key>;
 
-export type { DeepPartial, GetShape, ShapeConfig, RequireSingleField };
+export type { ShapeConfig, RequireSingleField };
